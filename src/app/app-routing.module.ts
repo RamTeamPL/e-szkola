d7d1004dae664f78cf50eebb50e4ed2b70697e1f
Antresko <barandawid80@gmail.com>
2024-04-06 18:36:38 +0200

    Sidebar01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,19 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'inbox',
+    loadChildren: () => import('./pages/inbox/inbox.module').then( m => m.InboxPageModule)
   },
+  {
+    path: 'outbox',
+    loadChildren: () => import('./pages/outbox/outbox.module').then( m => m.OutboxPageModule)
+  },
+  {
+    path: 'spam',
+    loadChildren: () => import('./pages/spam/spam.module').then( m => m.SpamPageModule)
+  },
+
 ];
 
 @NgModule({
