e1fbf861fab4b1cd10c37fd841c7339074e343ba
Antresko <barandawid80@gmail.com>
2024-04-06 23:06:52 +0200

    learn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,49 +9,68 @@ const routes: Routes = [
   },
   {
     path: 'competitions',
-    loadChildren: () => import('./pages/competitions/competitions.module').then( m => m.CompetitionsPageModule)
+    loadChildren: () =>
+      import('./pages/competitions/competitions.module').then(
+        (m) => m.CompetitionsPageModule
+      ),
   },
   {
     path: 'school-announcements',
-    loadChildren: () => import('./pages/school-announcements/school-announcements.module').then( m => m.SchoolAnnouncementsPageModule)
+    loadChildren: () =>
+      import('./pages/school-announcements/school-announcements.module').then(
+        (m) => m.SchoolAnnouncementsPageModule
+      ),
   },
   {
     path: 'class-announcements',
-    loadChildren: () => import('./pages/class-announcements/class-announcements.module').then( m => m.ClassAnnouncementsPageModule)
+    loadChildren: () =>
+      import('./pages/class-announcements/class-announcements.module').then(
+        (m) => m.ClassAnnouncementsPageModule
+      ),
   },
   {
     path: 'documents',
-    loadChildren: () => import('./pages/documents/documents.module').then( m => m.DocumentsPageModule)
+    loadChildren: () =>
+      import('./pages/documents/documents.module').then(
+        (m) => m.DocumentsPageModule
+      ),
   },
   {
     path: 'competitions',
-    loadChildren: () => import('./pages/competitions/competitions.module').then( m => m.CompetitionsPageModule)
+    loadChildren: () =>
+      import('./pages/competitions/competitions.module').then(
+        (m) => m.CompetitionsPageModule
+      ),
   },
   {
     path: 'fundraisers',
-    loadChildren: () => import('./pages/fundraisers/fundraisers.module').then( m => m.FundraisersPageModule)
+    loadChildren: () =>
+      import('./pages/fundraisers/fundraisers.module').then(
+        (m) => m.FundraisersPageModule
+      ),
   },
   {
     path: 'government',
-    loadChildren: () => import('./pages/government/government.module').then( m => m.GovernmentPageModule)
+    loadChildren: () =>
+      import('./pages/government/government.module').then(
+        (m) => m.GovernmentPageModule
+      ),
   },
   {
     path: 'trips',
-    loadChildren: () => import('./pages/trips/trips.module').then( m => m.TripsPageModule)
+    loadChildren: () =>
+      import('./pages/trips/trips.module').then((m) => m.TripsPageModule),
   },
   {
     path: 'learn',
-    loadChildren: () => import('./pages/learn/learn.module').then( m => m.LearnPageModule)
+    loadChildren: () =>
+      import('./pages/learn/learn.module').then((m) => m.LearnPageModule),
   },
   {
     path: 'surveys',
-    loadChildren: () => import('./pages/surveys/surveys.module').then( m => m.SurveysPageModule)
+    loadChildren: () =>
+      import('./pages/surveys/surveys.module').then((m) => m.SurveysPageModule),
   },
-
-
-];
-
-
 ];
 
 @NgModule({
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
