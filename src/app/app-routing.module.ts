a3ee521ca8d4bc570d46b56053bfeb10482fabae
Qrchy <130994567+Qrchy@users.noreply.github.com>
2024-04-07 00:41:15 +0200

    library added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,43 +9,71 @@ const routes: Routes = [
   },
   {
     path: 'competitions',
-    loadChildren: () => import('./pages/competitions/competitions.module').then( m => m.CompetitionsPageModule)
+    loadChildren: () =>
+      import('./pages/competitions/competitions.module').then(
+        (m) => m.CompetitionsPageModule
+      ),
   },
   {
     path: 'school-announcements',
-    loadChildren: () => import('./pages/school-announcements/school-announcements.module').then( m => m.SchoolAnnouncementsPageModule)
+    loadChildren: () =>
+      import('./pages/school-announcements/school-announcements.module').then(
+        (m) => m.SchoolAnnouncementsPageModule
+      ),
   },
   {
     path: 'class-announcements',
-    loadChildren: () => import('./pages/class-announcements/class-announcements.module').then( m => m.ClassAnnouncementsPageModule)
+    loadChildren: () =>
+      import('./pages/class-announcements/class-announcements.module').then(
+        (m) => m.ClassAnnouncementsPageModule
+      ),
   },
   {
     path: 'documents',
-    loadChildren: () => import('./pages/documents/documents.module').then( m => m.DocumentsPageModule)
+    loadChildren: () =>
+      import('./pages/documents/documents.module').then(
+        (m) => m.DocumentsPageModule
+      ),
   },
   {
     path: 'competitions',
-    loadChildren: () => import('./pages/competitions/competitions.module').then( m => m.CompetitionsPageModule)
+    loadChildren: () =>
+      import('./pages/competitions/competitions.module').then(
+        (m) => m.CompetitionsPageModule
+      ),
   },
   {
     path: 'fundraisers',
-    loadChildren: () => import('./pages/fundraisers/fundraisers.module').then( m => m.FundraisersPageModule)
+    loadChildren: () =>
+      import('./pages/fundraisers/fundraisers.module').then(
+        (m) => m.FundraisersPageModule
+      ),
   },
   {
     path: 'government',
-    loadChildren: () => import('./pages/government/government.module').then( m => m.GovernmentPageModule)
+    loadChildren: () =>
+      import('./pages/government/government.module').then(
+        (m) => m.GovernmentPageModule
+      ),
   },
   {
     path: 'trips',
-    loadChildren: () => import('./pages/trips/trips.module').then( m => m.TripsPageModule)
+    loadChildren: () =>
+      import('./pages/trips/trips.module').then((m) => m.TripsPageModule),
   },
   {
     path: 'learn',
-    loadChildren: () => import('./pages/learn/learn.module').then( m => m.LearnPageModule)
+    loadChildren: () =>
+      import('./pages/learn/learn.module').then((m) => m.LearnPageModule),
+  },
+  {
+    path: 'library',
+    loadChildren: () =>
+      import('./pages/library/library.module').then((m) => m.LibraryPageModule),
   },
 
-
 ];
+
 
 @NgModule({
   imports: [
