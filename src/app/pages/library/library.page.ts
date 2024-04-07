fe9545927eea9235d104e876154b3b7f89a92197
Qrchy <130994567+Qrchy@users.noreply.github.com>
2024-04-07 04:45:53 +0200

    books placed

--- src/app/pages/library/library.page.ts
+++ src/app/pages/library/library.page.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
+  // API_URL = "https://wolnelektury.pl/api/books/?format=api"
+  // data: any;
 
   ngOnInit() {
+    // fetch(this.API_URL)
+    //   .then((response) => response.json())
+    //   .then((this.data.slice(0, 20)))
+    //   .then((quotesData) => (this.data = quotesData));
+      
   }
+  searchTerm: string = '';
+
+  books = [
+    {title: 'Pan Tadeusz', author: 'Adam Mickiewicz', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/pan-tadeusz_1fNZm7P.jpg'},
+    {title: 'Quo Vadis', author: 'Henryk Sienkiewicz', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/quo-vadis_GDnK4W7.jpg'},
+    {title: 'Lord Jim', author: 'Joseph Conrad', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/lord-jim_Uu4uR1r.jpg'},
+    {title: 'Ludzie bezdomni', author: 'Stefan Żeromski', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/ludzie-bezdomni_OtChxtv.jpg'},
+    {title: 'Pan Tadeusz', author: 'Adam Mickiewicz', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/pan-tadeusz_1fNZm7P.jpg'},
+    {title: 'Quo Vadis', author: 'Henryk Sienkiewicz', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/quo-vadis_GDnK4W7.jpg'},
+    {title: 'Lord Jim', author: 'Joseph Conrad', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/lord-jim_Uu4uR1r.jpg'},
+    {title: 'Ludzie bezdomni', author: 'Stefan Żeromski', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/ludzie-bezdomni_OtChxtv.jpg'},
+    {title: 'Pan Tadeusz', author: 'Adam Mickiewicz', image: 'https://wolnelektury.pl/media/book/cover_api_thumb/pan-tadeusz_1fNZm7P.jpg'},
+  ]
+
+  searchBooks(): any[] {
+    return this.books.filter(book =>
+      book.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );}
+  // getHalfOfItems(): any[] {
+  //   const middleIndex = Math.ceil(this.books.length / 2);
+  //   return this.books.slice(0, middleIndex);
+  // }
+  
+
+  
 
 }
