9442c222f07a074a39df8c73af95e70fe6ee7d6d
unknown <mineomi1234@gmail.com>
2024-04-07 05:59:31 +0200

    Added ranking view

--- src/app/pages/score/score.page.ts
+++ src/app/pages/score/score.page.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScorePage implements OnInit {
 
-  constructor() { }
+  items = [
+    { 
+      name: 'Quiz z "Barok w Polsce")', 
+      students: [
+        "Jan Kowalski",
+        "Marek Sobuś",
+        "Krystian Baran"
+      ],
+      votes:[
+        '100%',
+        '83%',
+        '77%'
+      ]
+    },
+    { 
+      name: `Quiz z "Słówka unit'u 6.2"`, 
+      students: [
+        "Jan Kowalski",
+        "Marek Sobuś",
+        "Krystian Baran"
+      ],
+      votes:[
+        '95%',
+        '89%',
+        '67%'
+      ]
+    },
+    
+  ];
+  isOpen: boolean[] = [];
+
+  
+  constructor() {
+    this.items.forEach(() => this.isOpen.push(false));
+  }
+
+  toggleVotes(index: number) {
+    
+    this.isOpen[index] = !this.isOpen[index];
+  }
+
+  closeAllLists() {
+    this.isOpen.forEach((value, index) => {
+      this.isOpen[index] = false;
+    });
+  }
+  
 
   ngOnInit() {
+    
   }
 
 }
